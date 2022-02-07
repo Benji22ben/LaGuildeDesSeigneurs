260d94f81ec3c0ba3c59aacb0df29df5e433fbfd
Ben4600 <marques.benjaminb@gmail.com>
2022-02-07 15:36:05 +0100

    Adding Security

---
+++ src/Security/Voter/CharacterVoter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Character;
+use LogicException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class CharacterVoter extends Voter
+{
+    public const CHARACTER_DISPLAY = 'characterDisplay';
+
+    private const ATTRIBUTES = array(
+        self::CHARACTER_DISPLAY,
+    );
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        if(null !== $subject){
+            return $subject instanceof Character && in_array($attribute, self::ATTRIBUTES);
+        }
+
+        return in_array($attribute, self::ATTRIBUTES)
+            && $subject instanceof \App\Entity\Character;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'POST_EDIT':
+                // logic to determine if the user can EDIT
+                // return true or false
+                break;
+            case 'POST_VIEW':
+                // logic to determine if the user can VIEW
+                // return true or false
+                break;
+        }
+
+        return false;
+    }
+}
