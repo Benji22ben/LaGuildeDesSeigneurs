cbdd6798c157fb01a849d8dd5e43ac6b8c330e39
Ben4600 <marques.benjaminb@gmail.com>
2022-02-07 15:41:54 +0100

    Adding Security canDisplay ?

--- src/Security/Voter/CharacterVoter.php
+++ src/Security/Voter/CharacterVoter.php
@@ -28,24 +28,26 @@ class CharacterVoter extends Voter
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
     {
-        $user = $token->getUser();
-        // if the user is anonymous, do not grant access
-        if (!$user instanceof UserInterface) {
-            return false;
-        }
-
-        // ... (check conditions and return true to grant permission) ...
-        switch ($attribute) {
-            case 'POST_EDIT':
-                // logic to determine if the user can EDIT
-                // return true or false
-                break;
-            case 'POST_VIEW':
-                // logic to determine if the user can VIEW
-                // return true or false
+        //Define access rigts
+        switch($attribute){
+            case self::CHARACTER_DISPLAY:
+                //Peut envoyer $token et $subject pour tester des conditions
+                return $this->canDisplay();
                 break;
         }
-
-        return false;
+        // $user = $token->getUser();
+        // // if the user is anonymous, do not grant access
+        // if (!$user instanceof UserInterface) {
+        //     return false;
+        // }
+        throw new LogicException('Invalid attribute: ' . $attribute);
+    }
+    
+    /**
+     * Check if is allowed to display
+     */
+    private function canDisplay()
+    {
+        return true;
     }
 }
